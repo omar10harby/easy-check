5bb0f4a867659cfda22a79c2812980997ecc7386
omar <omar2002ua07@gmail.com>
2026-01-11 20:59:55 +0200

    Production ready

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAuthToken, removeAuthToken } from './authApi';
 
-const API_URL =  'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'http://158.220.126.228:8000';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
