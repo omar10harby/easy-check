e33eb8b5fc205c0f805186b70e226df4da6cbe88
omar <omar2002ua07@gmail.com>
2026-01-24 22:20:56 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import auth from '../features/auth/authSlice';
 import payment from '../features/payment/PaymentSlice';
 import imei from '../features/ImeiSearch/ImeiSlice';
+import user from "../features/user/UserSlice"
 export const store = configureStore({
   reducer: {
     auth,
     payment,
     imei,
+    user
   },
   devTools: import.meta.env.DEV, 
 });
