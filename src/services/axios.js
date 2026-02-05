ffe3de891a56e8a83eca4ca33cc2304891abf573
omar <omar2002ua07@gmail.com>
2026-02-05 20:24:10 +0200

    fix axios

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getAuthToken, removeAuthToken } from './authApi';
-const API_URL = "shaikly.com/api";
+const API_URL = "http://shaikly.com/api";
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
