dc95d06186e549ef9e43ceff9e43dd67ebc3bee0
omar <omar2002ua07@gmail.com>
2026-02-06 20:28:03 +0200

    add api url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { getAuthToken, removeAuthToken } from './authApi';
-const API_URL = "http://shaikly.com/api";
+const API_URL = import.meta.env.VITE_API_URL ;
 const axiosInstance = axios.create({
   baseURL: API_URL,
   headers: {
